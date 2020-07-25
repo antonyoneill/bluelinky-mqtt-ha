b6f37c22c2a05c04b0a2c8583f5be5f1a495f4c8
Antony O'Neill <antony@ant-web.co.uk>
2020-07-25 22:18:19 +0100

    Get vehicle status & pass to mqtt

---
+++ index.ts
@@ -0,0 +1,49 @@
+import * as BlueLinky from "bluelinky";
+import * as asyncMqtt from "async-mqtt";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+let bluelinkClient: any;
+let mqttClient: any;
+try {
+  bluelinkClient = new (BlueLinky as any)({
+    username: process.env.BL_USERNAME,
+    password: process.env.BL_PASSWORD,
+    region: process.env.BL_REGION,
+    pin: process.env.BL_PIN,
+  });
+
+  mqttClient = asyncMqtt.connect(process.env.MQTT_BROKER);
+
+  bluelinkClient.on("ready", async () => {
+    const vehicle = bluelinkClient.getVehicle(process.env.BL_VIN);
+
+    const status = await vehicle.status({ parsed: false });
+
+    await mqttClient.publish(
+      "kona/status",
+      JSON.stringify({
+        battery: status.evStatus.batteryStatus,
+        charging: status.evStatus.batteryCharge,
+        'battery-12v': status.battery.batSoc,
+        range: status.evStatus.reservChargeInfos.targetSOClist[0].dte.rangeByFuel.totalAvailableRange.value,
+        locked: status.doorLock
+      })
+    );
+
+    const location = await vehicle.location();
+
+    await mqttClient.publish(
+      "kona/location",
+      JSON.stringify({
+        longitude: location.longitude,
+        latitude: location.latitude,
+      })
+    );
+
+    mqttClient.end()
+  });
+} catch (err) {
+  console.error("Caught exception", err);
+}
